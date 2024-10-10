b3c70ad3826f93b6bbf735da24a901a7d605066d
MikeHatchi22 <hatchi.mike@gmail.com>
2024-10-10 12:34:50 +0200

    Testing card creation with JS

---
+++ script.js
@@ -0,0 +1,43 @@
+const person = [
+    {
+        name: "Abdou",
+        age: 24,
+        img: "./assets/avatar/abdou.jpg",
+        stack: "HTML",
+        description: "Abdou, jeune vieux de 38ans, soif de connaissances dans des domaines divers et variés. Eternel écrivain en herbe, j'écris souvent des éditos et analyses politiques à mes heures perdues. Passionné de la Tech, je me forge à explorer les métiers de la Tech dans une reconversion en cours.",
+        quote: "L'effort fait les forts",
+        levelHtml: 40,
+        levelCss: 40,
+        levelJs: 20,
+    },
+    {
+        name: "Abdou",
+        age: 24,
+        img: "./assets/avatar/abdou.jpg",
+        stack: "HTML",
+        description: "Abdou, jeune vieux de 38ans, soif de connaissances dans des domaines divers et variés. Eternel écrivain en herbe, j'écris souvent des éditos et analyses politiques à mes heures perdues. Passionné de la Tech, je me forge à explorer les métiers de la Tech dans une reconversion en cours.",
+        quote: "L'effort fait les forts",
+        levelHtml: 40,
+        levelCss: 40,
+        levelJs: 20,
+    },
+    {
+        name: "Abdou",
+        age: 24,
+        img: "./assets/avatar/abdou.jpg",
+        stack: "HTML",
+        description: "Abdou, jeune vieux de 38ans, soif de connaissances dans des domaines divers et variés. Eternel écrivain en herbe, j'écris souvent des éditos et analyses politiques à mes heures perdues. Passionné de la Tech, je me forge à explorer les métiers de la Tech dans une reconversion en cours.",
+        quote: "L'effort fait les forts",
+        levelHtml: 40,
+        levelCss: 40,
+        levelJs: 20,
+    }
+]
+
+const grid = document.querySelector('.grid')
+
+person.forEach((element) => {
+    const image = document.createElement('img')
+    grid.appendChild(image);
+    image.src = `${element.img}`;
+})
