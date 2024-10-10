6762f71aeaed89297e93c0f7de4152bb112f1cd9
MikeHatchi22 <hatchi.mike@gmail.com>
2024-10-10 14:06:11 +0200

    Change image card

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const person = [
     {
         name: "Abdou",
         age: 24,
-        img: "./assets/avatar/abdou.jpg",
+        img: "./assets/avatar/emma.jpg",
         stack: "HTML",
         description: "Abdou, jeune vieux de 38ans, soif de connaissances dans des domaines divers et variés. Eternel écrivain en herbe, j'écris souvent des éditos et analyses politiques à mes heures perdues. Passionné de la Tech, je me forge à explorer les métiers de la Tech dans une reconversion en cours.",
         quote: "L'effort fait les forts",
@@ -24,7 +24,7 @@ const person = [
     {
         name: "Abdou",
         age: 24,
-        img: "./assets/avatar/abdou.jpg",
+        img: "./assets/avatar/mike.jpg",
         stack: "HTML",
         description: "Abdou, jeune vieux de 38ans, soif de connaissances dans des domaines divers et variés. Eternel écrivain en herbe, j'écris souvent des éditos et analyses politiques à mes heures perdues. Passionné de la Tech, je me forge à explorer les métiers de la Tech dans une reconversion en cours.",
         quote: "L'effort fait les forts",
